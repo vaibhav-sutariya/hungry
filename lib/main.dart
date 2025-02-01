d9c7601ef69458205863a3e5deada5876dd96c69
vaibhav-sutariya <vsutariya428@gmail.com>
2025-02-01 15:28:31 +0530

    chore: update minSdkVersion to 23 and add Google Fonts to main theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:hungry/view/splash_screen.dart';
 
 void main() {
@@ -16,6 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        fontFamily: GoogleFonts.mulish().fontFamily,
       ),
       home: const SplashScreen(),
     );
