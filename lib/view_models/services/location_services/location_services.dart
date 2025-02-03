98809ef97f8c0f606a5677ba555b695f70579760
vaibhav-sutariya <vsutariya428@gmail.com>
2025-02-03 16:44:33 +0530

    feat: refactor AddressBox component; integrate LocationServices for location retrieval and loading state management

---
+++ lib/view_models/services/location_services/location_services.dart
@@ -0,0 +1,38 @@
+import 'dart:developer';
+
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:get/get.dart';
+
+class LocationServices extends GetxController {
+  RxBool isLoading = false.obs;
+  RxString currentAddress = 'geust'.obs;
+  void getCurrentLocation() async {
+    try {
+      isLoading.value = true;
+      Position position = await Geolocator.getCurrentPosition();
+
+      List<Placemark> placemarks =
+          await placemarkFromCoordinates(position.latitude, position.longitude);
+
+      if (placemarks.isNotEmpty) {
+        Placemark placemark = placemarks.first;
+        log(placemark.toString());
+        currentAddress.value = "${placemark.thoroughfare}, "
+            "${placemark.subLocality},"
+            " ${placemark.locality}\n"
+            "${placemark.administrativeArea}, "
+            "${placemark.subAdministrativeArea}"
+            "${placemark.country}, "
+            "${placemark.postalCode}";
+        log(currentAddress.value);
+      } else {
+        currentAddress.value = "Address not found";
+      }
+    } catch (e) {
+      print(e);
+    } finally {
+      isLoading.value = false;
+    }
+  }
+}
